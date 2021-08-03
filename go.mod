b59a561afae1b52e5997f2fdc9a5371f91c62baf
Enzo <1991996543@qq.com>
2021-08-03 18:09:49 +0800

    提取城市和URL

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module crawler
 go 1.16
 
 require (
-	golang.org/x/net v0.0.0-20210726213435-c6fcb2dbf985 // indirect
-	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/net v0.0.0-20210726213435-c6fcb2dbf985
+	golang.org/x/text v0.3.6
 )
--- main.go
+++ main.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 	"golang.org/x/net/html/charset"
 	"golang.org/x/text/encoding"
-	"golang.org/x/text/transform"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"regexp"
 )
 
 func main() {
@@ -22,13 +22,15 @@ func main() {
 		fmt.Println("Error: status code", resp.StatusCode)
 		return
 	}
-	e := determineEncoding(resp.Body)
-	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
-	all, err := ioutil.ReadAll(utf8Reader)
+	// TODO: 这里判断页面格式自动将其转为UTF8，但页面太大会存在溢出问题，后续处理先屏蔽了。
+	// e := determineEncoding(resp.Body)
+	// utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
+	all, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%s\n", all)
+	// fmt.Printf("%s\n", all)
+	printCityList(all)
 }
 
 func determineEncoding(r io.Reader) encoding.Encoding {
@@ -38,4 +40,14 @@ func determineEncoding(r io.Reader) encoding.Encoding {
 	}
 	e, _, _ := charset.DetermineEncoding(bytes, "")
 	return e
+}
+
+// printCityList 打印城市列表
+func printCityList(contents []byte) {
+	re := regexp.MustCompile(`<a href="(https?://[\w:/.]+")[^>]*>([^<]+)</a>`)
+	matches := re.FindAllSubmatch(contents, -1)
+	for _, m := range matches {
+		fmt.Printf("City: %s, URL: %s\n", m[2], m[1])
+	}
+	fmt.Printf("Matches found: %d\n", len(matches))
 }
