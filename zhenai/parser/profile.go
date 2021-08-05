52ab40ed368481abe2af7338f20b68a0c37124d7
Enzo <1991996543@qq.com>
2021-08-06 01:43:28 +0800

    增加用户信息解析器

---
+++ zhenai/parser/profile.go
@@ -0,0 +1,61 @@
+package parser
+
+import (
+	"crawler/engine"
+	"crawler/model"
+	"regexp"
+	"strconv"
+)
+
+var ageRe = regexp.MustCompile(`<td><span class="label">年龄：</span>(\d{1,3})岁</td>`)
+var heightRe = regexp.MustCompile(`<td><span class="label">身高：</span>(\d+)CM</td>`)
+var incomeRe = regexp.MustCompile(`<td><span class="label">月收入：</span>([^<]+)</td>`)
+var weightRe = regexp.MustCompile(`<td><span class="label">体重：</span><span field="">(\d+)KG</span></td>`)
+var genderRe = regexp.MustCompile(`<td><span class="label">性别：</span><span field="">([^<]+)</span></td>`)
+var xingzuoRe = regexp.MustCompile(`<td><span class="label">星座：</span><span field="">([^<]+)</span></td>`)
+var marriageRe = regexp.MustCompile(`<td><span class="label">婚况：</span>([^<]+)</td>`)
+var educationRe = regexp.MustCompile(`<td><span class="label">学历：</span>([^<]+)</td>`)
+var occupationRe = regexp.MustCompile(`<td><span class="label">职业：</span><span field="">([^<]+)</span></td>`)
+var hukouRe = regexp.MustCompile(`<td><span class="label">籍贯：</span>([^<]+)</td>`)
+var houseRe = regexp.MustCompile(`<td><span class="label">住房条件：</span><span field="">([^<]+)</span></td>`)
+var carRe = regexp.MustCompile(`<td><span class="label">是否购车：</span><span field="">([^<]+)</span></td>`)
+
+// ParseProfile 用户信息解析器.
+func ParseProfile(contents []byte) engine.ParseResult {
+	profile := model.Profile{}
+	profile.Age = extractInt(contents, ageRe)
+	profile.Height = extractInt(contents, heightRe)
+	profile.Weight = extractInt(contents, weightRe)
+	profile.Marriage = extractString(contents, marriageRe)
+	profile.Income = extractString(contents, incomeRe)
+	profile.Gender = extractString(contents, genderRe)
+	profile.Xingzuo = extractString(contents, xingzuoRe)
+	profile.Education = extractString(contents, educationRe)
+	profile.Occupation = extractString(contents, occupationRe)
+	profile.Hokou = extractString(contents, hukouRe)
+	profile.House = extractString(contents, houseRe)
+	profile.Car = extractString(contents, carRe)
+	result := engine.ParseResult{
+		Requests: nil,
+		Items:    []interface{}{profile},
+	}
+	return result
+}
+
+// extractString 提取字符串函数.
+func extractString(contents []byte, re *regexp.Regexp) string {
+	match := re.FindSubmatch(contents)
+	if len(match) > 1 {
+		return string(match[1])
+	}
+	return ""
+}
+
+// extractInt 提取整数函数.
+func extractInt(contents []byte, re *regexp.Regexp) int {
+	resultInt, err := strconv.Atoi(extractString(contents, re))
+	if err != nil {
+		return 0
+	}
+	return resultInt
+}
