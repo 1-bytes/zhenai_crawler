3e41085f7a35973370438340b144b446f42e7148
Enzo <1991996543@qq.com>
2021-08-08 00:07:33 +0800

    分页爬取

--- zhenai/parser/city.go
+++ zhenai/parser/city.go
@@ -5,12 +5,14 @@ import (
 	"regexp"
 )
 
-const cityRe = `<h2 class="post-title"><a href="(https?://[\w:/.]+)[^>]*">([^<]*)</a></h2>`
+var (
+	profileRe = regexp.MustCompile(`<h2 class="post-title"><a href="(https?://[\w:/.]+)[^>]*">([^<]*)</a></h2>`)
+	cityURLRe = regexp.MustCompile(`<span class="pager"><a href="(https?://[\w:/.]+)[^>]*">[^<]+</a></span>`)
+)
 
 // ParseCity 城市解析器.
 func ParseCity(contents []byte) engine.ParseResult {
-	re := regexp.MustCompile(cityRe)
-	matches := re.FindAllSubmatch(contents, -1)
+	matches := profileRe.FindAllSubmatch(contents, -1)
 	result := engine.ParseResult{}
 	for _, m := range matches {
 		name := string(m[2])
@@ -20,6 +22,14 @@ func ParseCity(contents []byte) engine.ParseResult {
 			ParserFunc: func(contents []byte) engine.ParseResult {
 				return ParseProfile(contents, name)
 			},
+		})
+	}
+
+	matches = cityURLRe.FindAllSubmatch(contents, -1)
+	for _, m := range matches {
+		result.Requests = append(result.Requests, engine.Request{
+			URL:        string(m[1]),
+			ParserFunc: ParseCity,
 		})
 	}
 	return result
